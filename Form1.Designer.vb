a51428de5314c55fc9d9c4bdb0a5da6c23f25347
Chaim gorbov <gchaimke@gmail.com>
2016-03-30 16:43:25 +0300

    job files add

--- Form1.Designer.vb
+++ Form1.Designer.vb
@@ -1,9 +1,9 @@
-﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
 Partial Class Form1
     Inherits System.Windows.Forms.Form
 
     'Form overrides dispose to clean up the component list.
-    <System.Diagnostics.DebuggerNonUserCode()> _
+    <System.Diagnostics.DebuggerNonUserCode()>
     Protected Overrides Sub Dispose(ByVal disposing As Boolean)
         Try
             If disposing AndAlso components IsNot Nothing Then
@@ -20,29 +20,21 @@ Partial Class Form1
     'NOTE: The following procedure is required by the Windows Form Designer
     'It can be modified using the Windows Form Designer.  
     'Do not modify it using the code editor.
-    <System.Diagnostics.DebuggerStepThrough()> _
+    <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
-        Me.ComboBox1 = New System.Windows.Forms.ComboBox()
         Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
         Me.FileToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
         Me.EditToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
         Me.HelpToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
         Me.Label1 = New System.Windows.Forms.Label()
         Me.Label2 = New System.Windows.Forms.Label()
-        Me.ListBox1 = New System.Windows.Forms.ListBox()
-        Me.TextBox1 = New System.Windows.Forms.TextBox()
-        Me.ListBox2 = New System.Windows.Forms.ListBox()
+        Me.LsbProjects = New System.Windows.Forms.ListBox()
+        Me.TxbFilter1 = New System.Windows.Forms.TextBox()
+        Me.LstJobs = New System.Windows.Forms.ListBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
         Me.MenuStrip1.SuspendLayout()
         Me.SuspendLayout()
-        '
-        'ComboBox1
-        '
-        Me.ComboBox1.FormattingEnabled = True
-        Me.ComboBox1.Location = New System.Drawing.Point(229, 59)
-        Me.ComboBox1.Margin = New System.Windows.Forms.Padding(2)
-        Me.ComboBox1.Name = "ComboBox1"
-        Me.ComboBox1.Size = New System.Drawing.Size(210, 21)
-        Me.ComboBox1.TabIndex = 0
         '
         'MenuStrip1
         '
@@ -51,7 +43,7 @@ Partial Class Form1
         Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
         Me.MenuStrip1.Name = "MenuStrip1"
         Me.MenuStrip1.Padding = New System.Windows.Forms.Padding(4, 2, 0, 2)
-        Me.MenuStrip1.Size = New System.Drawing.Size(446, 24)
+        Me.MenuStrip1.Size = New System.Drawing.Size(580, 24)
         Me.MenuStrip1.TabIndex = 4
         Me.MenuStrip1.Text = "MenuStrip1"
         '
@@ -93,40 +85,59 @@ Partial Class Form1
         Me.Label2.TabIndex = 10
         Me.Label2.Text = "Project Filter"
         '
-        'ListBox1
+        'LsbProjects
         '
-        Me.ListBox1.FormattingEnabled = True
-        Me.ListBox1.Location = New System.Drawing.Point(9, 97)
-        Me.ListBox1.Name = "ListBox1"
-        Me.ListBox1.Size = New System.Drawing.Size(100, 160)
-        Me.ListBox1.TabIndex = 15
+        Me.LsbProjects.FormattingEnabled = True
+        Me.LsbProjects.Location = New System.Drawing.Point(9, 97)
+        Me.LsbProjects.Name = "LsbProjects"
+        Me.LsbProjects.Size = New System.Drawing.Size(100, 160)
+        Me.LsbProjects.TabIndex = 15
         '
-        'TextBox1
+        'TxbFilter1
         '
-        Me.TextBox1.Location = New System.Drawing.Point(9, 59)
-        Me.TextBox1.Name = "TextBox1"
-        Me.TextBox1.Size = New System.Drawing.Size(100, 20)
-        Me.TextBox1.TabIndex = 16
+        Me.TxbFilter1.Location = New System.Drawing.Point(9, 59)
+        Me.TxbFilter1.Name = "TxbFilter1"
+        Me.TxbFilter1.Size = New System.Drawing.Size(100, 20)
+        Me.TxbFilter1.TabIndex = 16
         '
-        'ListBox2
+        'LstJobs
         '
-        Me.ListBox2.FormattingEnabled = True
-        Me.ListBox2.Location = New System.Drawing.Point(115, 97)
-        Me.ListBox2.Name = "ListBox2"
-        Me.ListBox2.Size = New System.Drawing.Size(195, 160)
-        Me.ListBox2.TabIndex = 17
+        Me.LstJobs.FormattingEnabled = True
+        Me.LstJobs.Location = New System.Drawing.Point(115, 97)
+        Me.LstJobs.Name = "LstJobs"
+        Me.LstJobs.Size = New System.Drawing.Size(195, 160)
+        Me.LstJobs.TabIndex = 17
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(332, 233)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(111, 23)
+        Me.Button1.TabIndex = 18
+        Me.Button1.Text = "New Project folder"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(332, 204)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(111, 23)
+        Me.Button2.TabIndex = 19
+        Me.Button2.Text = "New job file"
+        Me.Button2.UseVisualStyleBackColor = True
         '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(446, 273)
-        Me.Controls.Add(Me.ListBox2)
-        Me.Controls.Add(Me.TextBox1)
-        Me.Controls.Add(Me.ListBox1)
+        Me.ClientSize = New System.Drawing.Size(580, 286)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.LstJobs)
+        Me.Controls.Add(Me.TxbFilter1)
+        Me.Controls.Add(Me.LsbProjects)
         Me.Controls.Add(Me.Label2)
         Me.Controls.Add(Me.Label1)
-        Me.Controls.Add(Me.ComboBox1)
         Me.Controls.Add(Me.MenuStrip1)
         Me.MainMenuStrip = Me.MenuStrip1
         Me.Margin = New System.Windows.Forms.Padding(2)
@@ -138,15 +149,15 @@ Partial Class Form1
         Me.PerformLayout()
 
     End Sub
-
-    Friend WithEvents ComboBox1 As ComboBox
     Friend WithEvents MenuStrip1 As MenuStrip
     Friend WithEvents FileToolStripMenuItem As ToolStripMenuItem
     Friend WithEvents EditToolStripMenuItem As ToolStripMenuItem
     Friend WithEvents HelpToolStripMenuItem As ToolStripMenuItem
     Friend WithEvents Label1 As Label
     Friend WithEvents Label2 As Label
-    Friend WithEvents ListBox1 As ListBox
-    Friend WithEvents TextBox1 As TextBox
-    Friend WithEvents ListBox2 As ListBox
+    Friend WithEvents LsbProjects As ListBox
+    Friend WithEvents TxbFilter1 As TextBox
+    Friend WithEvents LstJobs As ListBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
 End Class
--- Form1.vb
+++ Form1.vb
@@ -1,16 +1,18 @@
-﻿Imports System
+﻿Option Explicit On
+Option Strict On
+
+Imports System
 Imports System.IO
 Imports System.Collections
+Imports System.Xml
 
 Public Class Form1
-    Dim MAIN_DIR_NAME As String = My.Computer.FileSystem.SpecialDirectories.MyDocuments & "\LM-job"
+    Private MAIN_DIR_NAME As String = My.Computer.FileSystem.SpecialDirectories.MyDocuments & "\LM-job"
+    Private LabelMarkPath As String = "C:\Program Files (x86)\Brady\LabelMark5\Bin\LM5.exe"
     Dim SelectedPath As String()
     Dim ProjectsDir As String()
     Dim CurrentProject As String
 
-    Private Sub Form1_Load_1(sender As Object, e As EventArgs) Handles MyBase.Load
-
-    End Sub
 
     Public Sub Main(ByVal args() As String)
         Dim path As String
@@ -19,61 +21,86 @@ Public Class Form1
                 ProcessFile(path)
             Else
                 If Directory.Exists(path) Then
-                    ProcessDirectory(path)
+                    ProcessDirectory()
                 Else
                     Console.WriteLine("{0} is not a valid file or directory.", path)
                 End If
             End If
         Next path
     End Sub 'Main
-    Public Sub ProcessDirectory(ByVal targetDirectory As String)
-        Dim fileEntries As String() = Directory.GetFiles(targetDirectory)
-        Dim fileName As String
-        For Each fileName In fileEntries
-            ProcessFile(fileName)
-        Next fileName
-        Dim subdirectoryEntries As String() = Directory.GetDirectories(targetDirectory)
-        For Each subdirectory In subdirectoryEntries
-            ProcessDirectory(subdirectory)
-            ListBox2.Items.Add(subdirectoryEntries)
-        Next subdirectory
+
+    Public Sub ProcessDirectory()
+        LsbProjects.Items.Clear()
+        Dim SearchFor As String = "*" & TxbFilter1.Text.ToString & "*"
+        Try
+            ProjectsDir = Directory.GetDirectories(MAIN_DIR_NAME, SearchFor)
+            Dim i As Integer
+            For Each dir As String In ProjectsDir
+
+                Dim dirInfo As New System.IO.DirectoryInfo(dir)
+                LsbProjects.Items.Add(dirInfo.Name)
+                i += 1
+            Next
+            Label1.Text = "Found =" & i
+        Catch ex As Exception
+            MsgBox(ex.Message.ToString)
+        End Try
     End Sub 'Processdirectory
 
     Public Sub ProcessFile(ByVal myPath As String)
         Console.WriteLine("Processed file '{0}'.", myPath)
         Dim fileName As String = Path.GetFileNameWithoutExtension(myPath)
-        ListBox2.Items.Add(fileName)
+        LstJobs.Items.Add(fileName)
     End Sub 'ProcessFile
 
-    Private Sub ListBox2_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ListBox2.SelectedIndexChanged
-        Dim OpenFile As String = MAIN_DIR_NAME & "\" & ListBox2.SelectedItem.ToString & ".lmj"
-        MsgBox(OpenFile)
+    Private Sub TxbFilter1_TextChanged(sender As Object, e As EventArgs) Handles TxbFilter1.TextChanged
+        ProcessDirectory()
+    End Sub
+
+    Private Sub LsbProjects_SelectedIndexChanged(sender As Object, e As EventArgs) Handles LsbProjects.SelectedIndexChanged
+        LstJobs.Items.Clear()
         Try
-            System.Diagnostics.Process.Start(OpenFile)
+            CurrentProject = MAIN_DIR_NAME & "\" & LsbProjects.SelectedItem.ToString
+            SelectedPath = Directory.GetFiles(CurrentProject)
+            Main(SelectedPath)
         Catch ex As Exception
-            MsgBox(ex.ToString)
+            MsgBox(ex.Message.ToString)
         End Try
 
     End Sub
 
-    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles TextBox1.TextChanged
-        ListBox1.Items.Clear()
-        Dim SearchFor As String = "*" & TextBox1.Text.ToString & "*"
-        ProjectsDir = Directory.GetDirectories(MAIN_DIR_NAME, SearchFor)
-        Dim i As Integer
-        For Each dir As String In ProjectsDir
-
-            Dim dirInfo As New System.IO.DirectoryInfo(dir)
-            ListBox1.Items.Add(dirInfo.Name)
-            i += 1
-        Next
-        Label1.Text = "Found =" & i
+    Private Sub LstJobs_SelectedIndexChanged(sender As Object, e As EventArgs) Handles LstJobs.SelectedIndexChanged
+        Dim OpenFile As String = MAIN_DIR_NAME & "\" & LsbProjects.SelectedItem.ToString & "\" & LstJobs.SelectedItem.ToString & ".lmj"
+        If MsgBoxResult.Ok = 1 Then
+            Try 'try to open file with LabelMark
+                'Create the XmlDocument.
+                Dim doc As New XmlDocument()
+                doc.Load(OpenFile)
+                'Display all the book titles.
+                Dim ReturnValue As New List(Of String)
+                For Each node As XmlNode In doc.SelectNodes("//LabelFile")
+                    LstJobs.Items.Add(node.Attributes("Path").Value)
+                Next
+                'System.Diagnostics.Process.Start(LabelMarkPath, OpenFile)
+            Catch ex As Exception
+                MsgBox(ex.Message.ToString)
+            End Try
+        End If
     End Sub
 
-    Private Sub ListBox1_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ListBox1.SelectedIndexChanged
-        CurrentProject = MAIN_DIR_NAME & "\" & ListBox1.SelectedItem.ToString
-        SelectedPath = Directory.GetFiles(CurrentProject)
-        Main(SelectedPath)
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim NewFolderName As String = MAIN_DIR_NAME & "\" & InputBox("Project Name", "Please Name new project folder")
+        Try
+            If (Not System.IO.Directory.Exists(NewFolderName)) Then
+                System.IO.Directory.CreateDirectory(NewFolderName)
+                MsgBox(NewFolderName & " created.")
+            Else
+                MsgBox("Folder " & NewFolderName & " exist!")
+            End If
+        Catch ex As Exception
+            MsgBox(ex.Message.ToString)
+        End Try
+
     End Sub
 
 End Class
